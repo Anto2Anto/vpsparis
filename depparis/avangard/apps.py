8d4fab8fc7226a511ad2e6ccc75f60558aa6a925
Anto2Anto <Anton.Suleimanov@gmail.com>
2022-12-25 23:49:29 +0500

    reinstall django project depparis ans reconfigure

--- depparis/avangard/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AvangardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'avangard'
-    verbose_name = 'Depannage'

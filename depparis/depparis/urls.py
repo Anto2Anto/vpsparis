f4747d4fdff1888ced82a42a899b376b1ed817c9
Anto2Anto <Anton.Suleimanov@gmail.com>
2022-12-26 18:49:57 +0500

    new app avangard & before copying from localhost(techartel)

--- depparis/depparis/urls.py
+++ depparis/depparis/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
+from depparis import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('avangard.urls')),
+    path('personal/', include('avangard.personal_urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
